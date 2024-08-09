f02cabf9dd9bea80e8579ab876b3906f2bcf0385
zhh2024 <15536508342@163.com>
2024-08-09 10:52:45 +0800

    提交

---
+++ src/main/java/com/zhang/jvmzhh/garbageCollection/StopTheWorldDemo.java
@@ -0,0 +1,60 @@
+package com.zhang.jvmzhh.garbageCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Desc:
+ * -Xms15m -Xmx15m
+ * list.clear()后,buffer对象没被任何对象引用,当新生代内存空间不足时,会触发垃圾回收
+ * @Author：zhh
+ * @Date：2024/8/8 17:28
+ */
+public class StopTheWorldDemo {
+    public static void main(String[] args) {
+        WorkThread w = new WorkThread();
+        PrintThread p = new PrintThread();
+        w.start();
+        p.start();
+    }
+
+    public static class WorkThread extends Thread {
+        List<byte[]> list = new ArrayList<byte[]>();
+
+        public void run() {
+            try {
+                while (true) {
+                    for(int i = 0;i < 1000;i++){
+                        byte[] buffer = new byte[1024];
+                        list.add(buffer);
+                    }
+
+                    if(list.size() > 10000){
+                        list.clear();
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static class PrintThread extends Thread {
+        public final long startTime = System.currentTimeMillis();
+
+        public void run() {
+            try {
+                while (true) {
+                    // 每秒打印时间信息
+                    long t = System.currentTimeMillis() - startTime;
+                    Thread.sleep(1000);
+                    System.out.println(t / 1000 + "." + t % 1000);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/com/zhang/jvmzhh/reference/SoftReferenceTest.java
@@ -0,0 +1,56 @@
+package com.zhang.jvmzhh.reference;
+
+import java.lang.ref.SoftReference;
+
+/**
+ * @Desc: 软引用
+ * -Xms10m -Xmx10m
+ * 当内存足够时，不会回收软引用可达的对象。内存不够时，会回收软引用的可达对象
+ * @Author：zhh
+ * @Date：2024/8/9 10:22
+ */
+public class SoftReferenceTest {
+    public static void main(String[] args) {
+        //创建对象，建立软引用
+        //SoftReference<User> userSoftRef = new SoftReference<User>(new User(1, "songhk"));
+        //上面的一行代码，等价于如下的三行代码
+        User u1 = new User(1,"songhk");
+        SoftReference<User> userSoftRef = new SoftReference<User>(u1);
+        //取消强引用
+        u1 = null;
+        //从软引用中重新获得强引用对象
+        System.out.println(userSoftRef.get());
+
+        System.out.println("---目前内存还不紧张---");
+        System.gc();
+        System.out.println("After GC:");
+        //垃圾回收之后获得软引用中的对象,由于堆空间内存足够，不会回收软引用的可达对象。
+        System.out.println(userSoftRef.get());
+        System.out.println("---下面开始内存紧张了---");
+        try {
+            //堆内存放不下,开始触发GC
+            byte[] b = new byte[1024 * 1024 * 10];
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            //再次从软引用中获取数据,在报OOM之前，垃圾回收器会回收软引用的可达对象。
+            System.out.println(userSoftRef.get());
+        }
+    }
+    public static class User {
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int id;
+        public String name;
+
+        @Override
+        public String toString() {
+            return "[id=" + id + ", name=" + name + "] ";
+        }
+    }
+
+
+}
